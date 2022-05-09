4de668677b47daff9c7166e0dff1d2b353857edb
pasinduG99 <it20223380@my.sliit.lk>
2022-05-09 14:16:32 +0530

    Created the code for save button

--- Practical/WebContent/Components/bills.js
+++ Practical/WebContent/Components/bills.js
@@ -3,4 +3,39 @@
  */
 
 
+$(document).on("click", "#btnSave", function(event)
+{
+	// Clear alerts---------------------
+	$("#alertSuccess").text("");
+	$("#alertSuccess").hide();
+	$("#alertError").text("");
+	$("#alertError").hide();
+	
+	
+	// Form validation-------------------
+	var status = validateItemForm();
+	if (status != true)
+	{
+		$("#alertError").text(status);
+		$("#alertError").show();
+		return;
+	}
+	
+	// If valid------------------------
+	var type = ($("#hidBillNOSave").val() == "") ? "POST" : "PUT";
+	
+	$.ajax(
+	{
+		url : "BillGenerationAPI",
+		type : type,
+		data : $("#formBill").serialize(),
+		dataType : "text",
+		complete : function(response, status)
+		{
+			onItemSaveComplete(response.responseText, status);
+		}
+	});
+});
+
+
 
